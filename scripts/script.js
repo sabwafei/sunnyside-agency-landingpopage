dc2e937a857513f9810f18384d32402ce9f30cd3
Christopher Smith <57920513+sabwafei@users.noreply.github.com>
2022-01-17 23:11:35 -0500

    nav interaction
    open and close mobile nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 // coded by SabWaFei
+//selectors
+//nav controls
+const mainNav = document.querySelector(".main-nav");
+const navBtn = document.querySelector(".hamburger-menu");
+//functions
+
+//event listeners
+// toggles mobile nav menu on/off
+navBtn.addEventListener("click", () => {
+  if (navBtn.classList.contains("hamburger-menu")) {
+    //toggle icon
+    navBtn.classList.replace("hamburger-menu", "fa-times");
+    //display nav menu
+    mainNav.style.display = "block";
+  } else {
+    //why is x symbol not showing
+    navBtn.classList.replace("fa-times", "hamburger-menu");
+    mainNav.style.display = "none";
+  }
+});
